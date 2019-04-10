a86ec08a09e15fcd03bb2f9c7b42e3bafad21b4e
Awez Shaikh <awez@awez.pnq-redhat.com>
2019-04-10 23:51:25 +0530

    helloworld program

---
+++ cpip/helloworld.py
@@ -0,0 +1,16 @@
+#Frist program to print and take argument from user
+
+print('Hello World!')                   # print on screen
+
+person = input('What\'s your name? ')    # take argument from user
+
+print('Hello,',person)                  # print message to taken argument aka input
+
+
+"""
+OUTPUT:
+
+Hello World!
+What's your name?Awez
+Hello, Awez
+"""
