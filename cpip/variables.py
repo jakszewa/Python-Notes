b06ba312a97eacde936b10bb2123485cf147a2c3
Awez Shaikh <awez@awez.pnq-redhat.com>
2019-04-10 23:59:31 +0530

    variable program

---
+++ cpip/variables.py
@@ -0,0 +1,10 @@
+# What is Variable and how to use it
+
+age = 12                # we are putting 12 in 'age' it holds the information
+
+print ('Age:',age)      # printing the value inside variable
+
+"""
+OUTPUT:
+Age: 12
+"""
