ceeda7be50f3d423106673bf5129cc6f10de73bc
Awez Shaikh <awez@awez.pnq-redhat.com>
2019-04-11 00:03:21 +0530

    Dos and don’ts

--- cpip/variables.py
+++ cpip/variables.py
@@ -8,3 +8,15 @@ print ('Age:',age)      # printing the value inside variable
 OUTPUT:
 Age: 12
 """
+
+'''
+Dos and don’ts
+• Start the variable’s name with a letter.
+• Any letter or number can be used in the name.
+• Symbols such as -, /, #, or @ aren’t allowed.
+• Spaces can’t be used.
+• An underscore ( _ ) can be used instead of a space.
+• Uppercase (capitals) and lowercase letters are different.
+  Python will treat “Score” and “score” as two different variables.
+• Avoid words Python uses as commands, such as “print”.
+'''
