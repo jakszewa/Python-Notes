ed7f033f6e5057a5479b4bfe7897b83d2d7434f6
Awez Shaikh <jakszewa@gmail.com>
2019-04-13 13:17:09 +0530

    Boolean Values

---
+++ cpip/conditions/boolean.py
@@ -0,0 +1,18 @@
+#Boolean Values
+
+'''
+The answers to the questions computers ask
+have only two possible values: True or False.
+Python calls these two values Boolean values,
+and they must always start with a capital letter.
+You can store a Boolean value in a variable.
+'''
+answer_one = True
+answer_two = False
+
+print(answer_one)
+
+x = 2
+print(x == 2) # prints out True
+print(x == 3) # prints out False
+print(x < 3) # prints out True
