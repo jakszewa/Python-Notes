73995345d1f838c6844e370cb016c1fa082b590d
Awez Shaikh <jakszewa@gmail.com>
2019-05-17 15:07:07 +0530

    number of days

--- cpip/functions/function3.py
+++ cpip/functions/function3.py
@@ -9,3 +9,12 @@ def print_seconds_per_day():
     return seconds
 
 print(print_seconds_per_day())
+
+def print_seconds_per_day(days):
+    hours = days * 24
+    minutes = hours * 60
+    seconds = minutes * 60
+
+    return seconds
+
+print(print_seconds_per_day(7))
