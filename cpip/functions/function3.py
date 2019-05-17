b355abd4fae18bc4915f6b552405c45bd51eaee6
Awez Shaikh <jakszewa@gmail.com>
2019-05-17 15:04:58 +0530

    function: calculate seconds

---
+++ cpip/functions/function3.py
@@ -0,0 +1,11 @@
+# create function
+# calculate seconds
+
+def print_seconds_per_day():
+    hours = 24
+    minutes = hours * 60
+    seconds = minutes * 60
+    
+    return seconds
+
+print(print_seconds_per_day())
