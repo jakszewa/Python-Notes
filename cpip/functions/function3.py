cebba7d04d06216819d5ae4dc7fb913143e29f5b
Awez Shaikh <jakszewa@gmail.com>
2019-05-17 15:14:07 +0530

    convert days to milliseconds

--- cpip/functions/function3.py
+++ cpip/functions/function3.py
@@ -10,11 +10,31 @@ def print_seconds_per_day():
 
 print(print_seconds_per_day())
 
-def print_seconds_per_day(days):
+# convert days to seconds
+
+def convert_days_to_seconds(days):
     hours = days * 24
     minutes = hours * 60
     seconds = minutes * 60
 
     return seconds
 
-print(print_seconds_per_day(7))
+print(convert_days_to_seconds(7))
+
+# convert days to milliseconds
+
+def convert_days_to_milliseconds(days):
+    hours = days * 24
+    minutes = hours * 60
+    seconds = minutes * 60
+
+    return seconds
+
+millisec = convert_days_to_milliseconds(7) * 1000
+
+print(millisec)
+
+
+
+
+
