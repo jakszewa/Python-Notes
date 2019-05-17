00d4a6186e41b0ca66716251c11d1584eaa748d7
Awez Shaikh <jakszewa@gmail.com>
2019-05-17 14:54:37 +0530

    replace function

--- cpip/functions/function2.py
+++ cpip/functions/function2.py
@@ -5,3 +5,13 @@ n = (10, 16, 30, 21, 25, 28)
 print(max(n))
 
 print(min(n))
+
+
+# replace function
+
+message = 'Python makes me Happy'
+
+print(message.replace('Happy', ':D'))
+
+print(message)
+
