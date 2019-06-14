71674671d423c739269f171f63d877ca8a70f86f
Awez Shaikh <jakszewa@gmail.com>
2019-06-15 01:49:23 +0530

    ex1.py: use of print function, ' & " too

---
+++ lpthw/ex1.py
@@ -0,0 +1,11 @@
+# TAGS: print function use
+# how to use print function to print anything
+# how to use ' & " combined
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this")
+print("This is fun")
+print('Yay! Printing')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
