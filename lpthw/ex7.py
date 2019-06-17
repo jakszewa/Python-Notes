28de730bf7a8e1b4c363393775bc2f027a39f51e
Awez Shaikh <jakszewa@gmail.com>
2019-06-17 21:05:31 +0530

    ex7: Practise of f function, .format()         Use of end=' '

--- lpthw/ex7.py
+++ lpthw/ex7.py
@@ -1,1 +1,23 @@
-# TAGS: 
+# TAGS: strings, f function, .format()
+# Practise of f function, .format()
+
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10)     # prints . 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')     # end=' ' combines two lines output and put space between them if given
+print(end7 + end8 + end9 + end10 + end11 + end12)
