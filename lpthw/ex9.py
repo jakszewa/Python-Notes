eca075b03e538c389bfe3f63613e3a4e934ae565
Awez Shaikh <jakszewa@gmail.com>
2019-06-19 17:27:47 +0530

    ex9: use of \n , """

--- lpthw/ex9.py
+++ lpthw/ex9.py
@@ -1,1 +1,14 @@
-# TAGS: 
+# TAGS: \n , """
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"     # \n is for new line
+
+print("Here are the days:", days)
+print("Here are the months:",months)
+
+print("""                                                
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6
+""")                                        # """ are for long comments and printing each string on new line 
