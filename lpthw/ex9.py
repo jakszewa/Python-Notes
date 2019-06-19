4ce29ada904d3728bc32e1c13e967539b364f489
Awez Shaikh <jakszewa@gmail.com>
2019-06-19 17:33:32 +0530

    ex9: use of \n and """

--- lpthw/ex9.py
+++ lpthw/ex9.py
@@ -11,6 +11,5 @@ There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6
-<<<<<<< HEAD
 """)                                        # """ are for long comments and printing each string on new line 
 
