2b2f8896a5e4e124ab8bd29d7fd7acfda7cda809
Awez Shaikh <jakszewa@gmail.com>
2019-06-20 17:15:47 +0530

    ex10:  Use of  \t \n """

--- lpthw/ex10.py
+++ lpthw/ex10.py
@@ -1,1 +1,20 @@
-# TAGS: 
+# TAGS: \t \n """
+
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backlash_cat)
+print(fat_cat)
+
+print(tabby_cat.format(persian_cat))
