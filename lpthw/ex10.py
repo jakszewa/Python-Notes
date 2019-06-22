51a5c12bef5e408414bdbc79893fecd6c2e22265
Awez Shaikh <jakszewa@gmail.com>
2019-06-23 00:14:35 +0530

    ex10: \t \n """

--- lpthw/ex10.py
+++ lpthw/ex10.py
@@ -1,20 +1,28 @@
 # TAGS: \t \n """
 
 
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backlash_cat = "I'm \\ a \\ cat."
+tabby_cat = "\tI'm tabbed in."          # Adding TAB in output
+persian_cat = "I'm split\non a line."   # Splitting to new line
+backlash_cat = "I'm \\ a \\ cat."       # escapting sequence
 
 fat_cat = """
 I'll do a list:
 \t* Cat food
 \t* Fishes
 \t* Catnip\n\t* Grass
-"""
-
+"""                                     # Using """ to split on new line
 print(tabby_cat)
 print(persian_cat)
 print(backlash_cat)
 print(fat_cat)
 
 print(tabby_cat.format(persian_cat))
+
+testing = '''
+There 
+are 
+single 
+quoates
+'''                                     # ''' can also be used instead of """
+
+print(testing)
