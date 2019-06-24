a7c84753322ae4eb592c3e8593db9a68acaf948d
Awez Shaikh <jakszewa@gmail.com>
2019-06-24 18:12:56 +0530

    ex13: Module sys, argv:  command-line arguments Example

---
+++ lpthw/ex13.py
@@ -0,0 +1,13 @@
+# TAGS: Module sys, argv:  command-line arguments 
+
+from sys import argv
+
+script, first, second, thrird = argv
+
+
+print("The script is called: ",script)
+print("Your first variable is: ", first)
+print("Your second variable is: ",second)
+print("Your third variable is: ",thrird)
+
+
