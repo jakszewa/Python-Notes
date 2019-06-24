3926053c1c29856c32f6925914ea0b0ef5413b54
Awez Shaikh <jakszewa@gmail.com>
2019-06-24 18:12:32 +0530

    ex12: input(), end=' ' example

---
+++ lpthw/ex12.py
@@ -0,0 +1,16 @@
+# TAGS: input(), end=' '
+# () <-- parenthesis
+
+
+
+age = input("How old are you? ")                # you can put what you wanna ask inside ()
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
+
+#print("How old are you? ", input())            
+''' 
+this is not working because input() is getting excuted first then
+print function
+'''
